b43dbe8ab1b66a2d79ed50f116be21dc864e94d2
Trinidad Padilla <tpadilla101@live.com>
2022-02-25 16:19:12 -0800

    got modal working

--- src/frontend/components/layout/modal/Modal.jsx
+++ src/frontend/components/layout/modal/Modal.jsx
@@ -1,17 +1,36 @@
 import React, { useEffect, useRef } from 'react';
-import './Modal.scss';
+import { useDispatch } from 'react-redux';
+import { handleModalVisibility } from '../../../reduxslices/modalSlice';
+
 
 const Modal = ( {children} ) => {
-    const modalReference = null;
+    const modalReference = useRef(null);
+    const dispatch = useDispatch();
 
+        const modalToggle = () => {
+            dispatch(handleModalVisibility())
+            console.log('clicked');
+        };
+/* Allows me to click anywhere outside the Modal to close it: */
 
+   /*  useEffect( () => {
+
+        function handleClick(event) {
+            if(!modalReference.current?.contains(event.target)) {
+                modalToggle();
+            }
+        }
+
+        document.addEventListener("click", handleClick);
+        return () => document.removeEventListener("click", handleClick);
+    }, ); */
 
   return (
       
     <section className="modal-parent-container" role="dialog" aria-modal="true">
-        <div className='modal-content'>
+        <div className='modal-content' ref={modalReference}>
             <div className='modal-header'>
-                <button className='modal-close-button'>
+                <button className='modal-close-button' title="Close modal" onClick={modalToggle}>
                     x
                 </button>
             </div>
