0aa1efca01d0135fd8cf1cf13e08c3263feefae2
Trinidad Padilla <tpadilla101@live.com>
2022-02-28 16:42:43 -0800

    testing the login from UI

--- src/frontend/components/layout/login/Login.jsx
+++ src/frontend/components/layout/login/Login.jsx
@@ -1,24 +1,40 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { CtaButton } from '../../utils';
 import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../../../reduxslices/authSlice';
+import { clearMessage } from '../../../reduxslices/authmessageSlice';
 import cabana from '../../../assets/images/cabana.jpg';
 
-function Login() {
+function Login(props) {
 
+  const { isLoggedIn } = useSelector( (state) => state.auth);
+  
+  const { message } = useSelector( (state) => state.message);
+
+  const dispatch = useDispatch();
   const formRef = useRef();
 
-  /* const loginHandler = async(event) => {
+  useEffect( () => {
+    dispatch(clearMessage());
+  }, [dispatch]);
 
-    try {
-      event.PreventDefault();
-      const response = await axios.post(`/api/users/login`, {username, password});
+  const initialValues = {
+    username: "",
+    password: "",
+  };
 
+  const handleLogin = (formValue) => {
+    const { username, password } = formValue;
 
-    } catch (error) {
-      console.log(error);
-    }
-  }; */
+    dispatch(login( { username, password }))
+    /* .unwrap() */
+    /* .then( () => {
+
+    }) */
+  };
+
+  
 
   return (
     <section className='login-parent-container'>
@@ -27,7 +43,7 @@ function Login() {
       {/* <img className='login-bg-img' src={cabana} alt='A cabana along a beach'></img>
  */}
     {/* TODO: form needs an onSubmit event handler */}
-      <form ref={formRef} className="login-form-parent-container">
+      <form ref={formRef} className="login-form-parent-container" onSubmit={handleLogin}>
         <h1 className='login-form-h1'>Sign in to Shop</h1>
         <h3 className='login-form-h3'>Please sign in to your account</h3>
         <div className='login-form-input-wrapper'> {/* TODO: Inputs need onChange events to capture entered value */}
@@ -38,6 +54,14 @@ function Login() {
           <CtaButton text={'Sign in'} />
 
         </div>
+        
+        {message && (
+          <div>
+            <div className='alert alert-danger' role='alert'>
+              {message}
+            </div>
+          </div>
+        )}
 
       </form>
 
