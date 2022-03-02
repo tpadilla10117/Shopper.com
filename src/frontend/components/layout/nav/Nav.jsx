3c571409facbd84974668c083c6d85b82e0b4cbd
Trinidad Padilla <tpadilla101@live.com>
2022-03-01 20:29:07 -0800

    got the shopping cart icon styled, though not dynamically

--- src/frontend/components/layout/nav/Nav.jsx
+++ src/frontend/components/layout/nav/Nav.jsx
@@ -78,10 +78,19 @@ const Nav = (props) => {
 
         </div>
 
+
         {/* TODO: For the mobile Icon */}
         <div className='nav-mobile-icon-container'>
 
             <h1 className='nav-mobile-header'>Shop</h1>
+
+            <div className='nav-mobile-shoppingcart-wrapper'>
+                <span className='nav-mobile-shoppingcart-counter'>1</span>
+
+                <svg xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24" stroke="currentColor" height="34" width="24"className='nav-mobile-shoppingcart-icon'>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg>
+            </div>
 
             <svg xmlns="http://www.w3.org/2000/svg" className="nav-mobile-icon" fill="none" height="24" width="24" viewBox="0 0 24 24" stroke="currentColor" onClick={navToggle} alt="A three-lined horizontal icon to toggle a navigation menu">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
