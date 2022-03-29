3502331ea4b1b9fbd8d24d01b8e7c0a6d075e951
Trinidad Padilla <tpadilla101@live.com>
2022-03-29 16:57:57 -0700

    working on the stripe checkout session in stripeCheckout.js

--- src/frontend/api/stripeCheckout.js
+++ src/frontend/api/stripeCheckout.js
@@ -1,1 +1,31 @@
-/* Where I create a stripe checkout session: */
+/* TODO: Where I create a stripe checkout session: */
+
+    /* import { useSelector } from 'react-redux';
+    import { selectItems, selectTotal } from '../reduxslices/basketslice'; */
+
+    const stripeApi = require('./stripe');
+
+    async function createStripeCheckoutSession(req, res) {
+        const domainURL = process.env.WEB_APP_URL;
+        const { line_items, customer_email } = req.body;
+
+    /* Check if req.body has items and email: */
+        if(!line_items || customer_email) {
+            return res.status(400).json({error: 'Missing required session parameters!'})
+        };
+
+        let session;
+
+        try {
+            session = await stripeApi.checkout.sessions.create({
+                payment_method_types: ['card'],
+                mode: 'payment',
+                line_items,
+                customer_email,
+                success_url: ``
+
+            })
+        } catch (error) {
+
+        }
+    };
