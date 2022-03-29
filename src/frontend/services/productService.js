a8e7aa5b9a428402a8143ba7f7fe48894abbc00d
Trinidad Padilla <tpadilla101@live.com>
2022-03-28 17:36:07 -0700

    need to conditionally render the api call

--- src/frontend/services/productService.js
+++ src/frontend/services/productService.js
@@ -4,10 +4,12 @@
 
     /* const FAKESTORE_API_URL = "https://fakestoreapi.com/"; */
     const MY_API_URL = "http://localhost:3000/api/products"
+    
 
 /* Logic to request products from fakestoreapi.com : */
     const productRequest = () => {
         /* return axios.get(FAKESTORE_API_URL + "products?limit=5") */
+      /* TODO: May need to write conditional logic so api call runs only once */  
         return axios.get(MY_API_URL)
         .then( (response) => {
             console.log(response.data)
