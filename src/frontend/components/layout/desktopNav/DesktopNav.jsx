8e20145fc3cc164d388e0b8af6d5661c6f14b6c2
Trinidad Padilla <tpadilla101@live.com>
2022-05-29 14:53:17 -0700

    styled most of the navbar

--- src/frontend/components/layout/desktopNav/DesktopNav.jsx
+++ src/frontend/components/layout/desktopNav/DesktopNav.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { NavbarData } from '../../../seed';
 import { NavLink } from 'react-router-dom';
 import '../../App.scss';
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 
 const DesktopNav = (props) => {
 
@@ -9,28 +10,54 @@ const DesktopNav = (props) => {
   return (
     <nav className='desktopNav-parent-container'>
         <div className='desktop-nav-items-container'>
-            {NavbarData.map( (item, index) => {
-                return (
-                    <NavLink 
-                        to={item.path}
-                        className='desktopNav-nav-item'
-                        exact="true"
-                        activeclassname='active'
-                        style={ {textDecoration: 'none'}}
-                        key={index}
-                    >
-                        <ul>
-                            <li
-                                key={index}
-                                className={item.title}
-                                id='navlinks'
-                            >
-                                <span>{item.title}</span>
-                            </li>
-                        </ul>
-                    </NavLink>
-                )
-            })}
+            
+
+            <div className='desktop-nav-items-left'>
+                <span className='desktop-nav-items-languages' >EN</span>
+                <div className='desktop-nav-items-search-container'>
+                    <input placeholder='Search' />
+                    <Search style={{ color: "gray", fontSize: 16 }} />
+                </div>
+            </div>
+
+            <div className='desktop-nav-items-center'>
+                <h1 className='desktop-nav-items-center-h1'>SHOPPER</h1>
+            </div>
+
+            <div className='desktop-nav-items-right'>
+                <NavLink
+                    to='/signin'
+                    className='desktopNav-nav-item'
+                    exact="true"
+                    activeclassname='active'
+                    style={ {textDecoration: 'none'}}
+                >
+                    REGISTER
+                </NavLink>
+                <NavLink
+                    to='/signin'
+                    className='desktopNav-nav-item'
+                    exact="true"
+                    activeclassname='active'
+                    style={ {textDecoration: 'none'}}
+                >
+                    LOGIN
+                </NavLink>
+                <NavLink
+                    to='/checkout'
+                    className='desktopNav-nav-item'
+                    exact="true"
+                    activeclassname='active'
+                    style={ {textDecoration: 'none'}}
+                >
+                    {/* <Badge badgeContent={4} color='primary'> */}
+                        <ShoppingCartOutlined />
+                    {/* </Badge> */}
+                </NavLink>
+            </div>
+
+
+
         </div>
 
 
