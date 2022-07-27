63dcf3f03184b2b8f0792cade015dcb446c8965a
Trinidad Padilla <tpadilla101@live.com>
2022-07-26 19:09:13 -0700

    added spinner

---
+++ src/frontend/components/layout/spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.scss';
+
+function Spinner() {
+  return (
+    <div className='spinner-overlay'>
+        <div className='spinner-component-container'></div>
+    </div>
+  )
+}
+
+export default Spinner;
