a40d03e213706f18f13ae79377b0e7aea7c76c34
Trinidad Padilla <tpadilla101@live.com>
2022-08-22 19:40:07 -0700

    see comments

--- src/frontend/components/pages/saveditemspg/SavedItemsPg.jsx
+++ src/frontend/components/pages/saveditemspg/SavedItemsPg.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { retrieveUsersSavedItems, selectUsersSavedItems } from '../../../reduxslices/savedItemsSlice';
+import { selectItems } from '../../../reduxslices/productSlice';
 import { userData } from '../../../reduxslices/authSlice';
 
 import {
@@ -14,20 +15,27 @@ import {
 function SavedItemsPg() {
 
     const dispatch = useDispatch();
+/* TODO: NEED to comnpare the items in all products to the ones in savedITems, or find some way to retireve the info from products tabl;e:  */
     const user = useSelector(userData);
+    const allProducts = useSelector(selectItems);
+    const usersSavedItems = useSelector(selectUsersSavedItems);
+    /* console.log(usersSavedItems) */
     
-    const location = useLocation();
-    console.log(location)
+    
+    /* const location = useLocation();
+    console.log(location) */
+
+    
+
 /* TODO: Need to only render the existing savedItems once, and then re-render if additional items added */
-   /*  useEffect( () => {
-        if(user) {
+    useEffect( () => {
+        if(usersSavedItems.length === 0 && user) {
             dispatch(retrieveUsersSavedItems())
         }
         console.log('from the savedItems page useEffect')
-    }, [dispatch, user]) */
+    }, [dispatch, user, usersSavedItems])
 
-    const usersSavedItems = useSelector(selectUsersSavedItems);
-    console.log(usersSavedItems)
+    
 
 
     return (
@@ -47,6 +55,7 @@ function SavedItemsPg() {
                     return (
                         <div id={items.id} key={items.id}>
                             <p>{items.product_id}</p>
+                            {/* {filterSavedProductsOutOfAllProducts(items.product_id)} */}
                         </div>
                     )
                 }
