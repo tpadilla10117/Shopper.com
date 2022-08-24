d957aefe3586fd1841e195807191dc85e67fb00e
Trinidad Padilla <tpadilla101@live.com>
2022-08-24 09:43:00 -0700

    fixed the join statement for saved_products.js, getSavedProductsByUserId

--- src/backend/dbadapters/saved_products.js
+++ src/backend/dbadapters/saved_products.js
@@ -42,27 +42,22 @@ const {
         }
     };
 
+/* Retrieve saved items for a user: */
     async function getSavedProductsByUserId(user_id) {
         try {
+          
             const { rows: saved_products } = await client.query(`
-                SELECT * FROM saved_products
+                SELECT * FROM products
+                INNER JOIN saved_products ON saved_products.product_id = products.id
                 WHERE saved_products.user_id = $1
             `, [user_id] );
 
-        /* Have to retrieve product information based on the id (primary key): */
-
-            const allProductInformation = await Promise.all(saved_products.map( element => getProductById(element.product_id)));
-
-            
-            return allProductInformation;
-
-            /* return saved_products; */
+            return saved_products;
             
         } catch(error) {
             throw error;
         }
     };
-
 
 module.exports = {
     createSavedProduct,
