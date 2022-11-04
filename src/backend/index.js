765124c1accb5aa682e952b7983cadd696640aac
shapirodaniel <shapirodanieladam@gmail.com>
2022-11-04 17:08:56 -0400

    update: postgres db url

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,14 +1,14 @@
 /* This is where connection to PostgreSQL client occurs: */
-    
-    require('dotenv').config();
 
-    //1) Connect client to the PostgreSQL db:
-    const { Client } = require('pg');
+require('dotenv').config();
 
-    //2) Give a name to the db, and a location (check .env file): 
-    
-    const DB_URL = `postgres://${process.env.REACT_APP_DB_NAME}`;
-    const client = new Client(DB_URL);
+//1) Connect client to the PostgreSQL db:
+const { Client } = require('pg');
 
-    // Exports:
-    module.exports = {client};
+//2) Give a name to the db, and a location (check .env file):
+
+const DB_URL = `postgres://localhost:5432/${process.env.REACT_APP_DB_NAME}`;
+const client = new Client(DB_URL);
+
+// Exports:
+module.exports = { client };
