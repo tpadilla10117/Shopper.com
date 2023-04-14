be94ae02db591d1b49656b2395dd1d50f7d7f395
Trinidad Padilla <tpadilla101@live.com>
2023-04-13 17:23:17 -0700

    working on caching with redis

--- backend/api/shop.js
+++ backend/api/shop.js
@@ -1,18 +1,61 @@
 /* Routing for products: */
 
 import express from 'express';
+/* import apicache from 'apicache'; */
+import { createClient} from 'redis';
 
 /* Import DB methods: */
 import { getAllProducts, getProductById } from '../db/dbadapters/products.js';
 
 export const shopRouter = express.Router();
 
+/* Create a Redis Client: */
+
+/* const cache = apicache.middleware; */
+
+/* By default, Redis client will try to connect to 'localhost' on port 6379: */
+const client = createClient({
+	host: 'localhost',
+	port: 6379,
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+
+/* Create middleware that checks if the requested data is available in Redis cache: */
+
+const cache = (req, res, next) => {
+  
+	client.get(req.originalUrl, (err, data) => {
+	  if (err) throw err;
+  
+	  if (data !== null) {
+		res.send(JSON.parse(data));
+	  } else {
+		next();
+	  }
+	});
+};
+
+console.log("Here is cache: ", cache);
+
 /* ------------------------------------------------------------ */
 /* THIS IS THE GET/products ROUTER */
+/* TODO:
 
-shopRouter.get('/', async (req, res, next) => {
+	- Try creating  a new funciton called getProducts(), that receives an object that is essentially the filtered data I want
+	- 
+*/
+
+shopRouter.get('/', cache, async (req, res, next) => {
+	const { page = 4, limit = 5 } = req.query;
+
 	try {
 		const products = await getAllProducts();
+
+		client.setex(req.originalUrl, 3600, JSON.stringify(products));
+
 		res.send(products);
 		return products;
 	} catch (error) {
